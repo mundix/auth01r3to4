696ef52262e1dd9ecf1a218c6984c306ca4f58aa
Edmundo Pichardo <mundix@Edmundos-MacBook-Pro.local>
2016-01-03 12:20:54 -0400

    Se generaron los metodos de los modelos y los callbacks falta instalar la gema de bcrypt para el salt encryptation, y las vistas de new mas el controller estan hechos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    # Asumo que params[:user] es el formulario
+    @user = User.new(params[:user])
+
+    if @user.save
+      flash[:notice] = "Usd. ingresó correctamente."
+      flash[:color] = "valid"
+    else
+      flash[:notice] = "Usd. no pudo ingresar ."
+      flash[:color] = "invalid"
+    end
+    # Renderiza la vista new
+    render "new"
   end
 end
