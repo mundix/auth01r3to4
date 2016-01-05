d8fd15d8b4111d8c268e7c973dfcaa56d3e520ce
Edmundo Pichardo <mundix@Edmundos-MacBook-Pro.local>
2016-01-05 07:09:20 -0400

    lsto para subir, se registra y valida todo mas las gemas instaladas

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   attr_accessor :password
 
-  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
 
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
